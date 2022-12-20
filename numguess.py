1d997936120bf9599788948e2c2aba7613e3a999
Murtaza Ali <murtaza.ali3221@gmail.com>
2022-12-19 22:10:34 -0500

    Created numguess.py
    Staring commit

---
+++ numguess.py
@@ -0,0 +1,18 @@
+def main():
+    print("Inside main()")
+    number = number_input()
+
+def guess_number(number):
+    guess = False
+    while not guess:
+        if answer = "higher":
+            guess = guess + 1
+    return number
+def number_input():
+    print("Please input a number for the computer to guess between 1 and 100")
+    number = int(input())
+    return number
+if __name__ == "__main__":
+    print("Welcome to the number guessing game! You will be asked to insert a number for the computer to guess")
+
+    main()
