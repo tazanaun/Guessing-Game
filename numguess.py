dead3099739dc7f3a936fbef44e038965d0f1188
Murtaza Ali <murtaza.ali3221@gmail.com>
2022-12-19 22:31:20 -0500

    Fixed error with mid point calculation

--- numguess.py
+++ numguess.py
@@ -1,21 +1,30 @@
 def main():
     print("Inside main()")
     number = number_input()
-
+    print(guess_number(number))
 def guess_number(number):
     isGuess = False
+    max = 100
+    min = 0
+    mid = max/2
     while not isGuess:
-        guess = 50
-        print("Is your number", guess, "?")
+        
+        print("Is your number", mid, "?")
         response = input()
+        
 
         if response == "yes":
             isGuess = True
+            print("The computer guessed your number!")
         elif response == "lower":
-            guess = guess/2
+            max = mid
+            mid = (max+min)/2
         elif response == "higher":
-            guess = guess + 1
-    return number
+            #wont work rn
+            min = mid
+            mid = (max+min)/2
+    return mid
+    
 def number_input():
     print("Please input a number for the computer to guess between 1 and 100")
     number = int(input())
